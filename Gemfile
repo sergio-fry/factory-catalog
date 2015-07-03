06fb21ed9f44de071109b08d1ad1d89fa7747a1d
Sergei O. Udalov <sergei.udalov@gmail.com>
2015-07-03 13:00:09 +0300

    assets memcache

--- Gemfile
+++ Gemfile
@@ -53,6 +53,8 @@ end
 
 group :production do
   gem 'rails_12factor'
+  gem 'dalli'
+  gem 'memcachier'
 end
 
 gem 'rails_admin'
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.assets.cache_store = :dalli_store
 end
